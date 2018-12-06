dc626aa457264ffacd11280004da3a8ea51e376d
lbouri <laurent.bouri@france-bioinformatique.fr>
2018-12-06 16:27:05 +0100

    commit catalogue

--- catalogue/settings.py
+++ catalogue/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,9 +84,9 @@ WSGI_APPLICATION = 'catalogue.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catalogue',
-        'USER': 'lbouri',
-        'PASSWORD': 'Samba35',
+        'NAME': database_name,
+        'USER': database_user,
+        'PASSWORD': database_password,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -125,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '.static'
--- portal/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Index page for Catalogue of French resources in bioinformatics")
