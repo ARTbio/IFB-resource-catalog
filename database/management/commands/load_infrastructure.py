58b9132a620c6b1b8765e569255b45dce1c99872
lbouri <laurent.bouri@france-bioinformatique.fr>
2019-10-07 17:28:26 +0200

    models update

--- database/management/commands/load_infrastructure.py
+++ database/management/commands/load_infrastructure.py
@@ -1,6 +1,7 @@
 
 import os
 import csv
+import re
 
 from django.core.management import BaseCommand
 from database.models import Platform
@@ -21,11 +22,11 @@ class Command(BaseCommand):
                     pf_name = data_object[0]
                     pf_infrastructure_type = data_object[1]
                     pf_useful_storage_capacity = data_object[2]
-                    pf_cpu_number = data_object[3].split(" ")[0] or 0
+                    pf_cpu_number = data_object[3].replace(" cores","").replace(" ", "") or 0
                     pf_data_collection = data_object[4]
                     pf_cpu_hour_per_year = data_object[5]
                     pf_informatics_tools = data_object[6]
-                    pf_users_number = data_object[7]
+                    pf_users_number = data_object[7].replace(" ","") or 0
                     pf_support_condition = data_object[8]
                     pf_server_description = data_object[9]
                     pf_title_project_support = data_object[10]
@@ -35,25 +36,23 @@ class Command(BaseCommand):
                     pf_publications = data_object[14]
                     platform = ""
                     try:
-                        if platform.objects.get(platform=pf_name).exists():
-                            platform, created = Platform.objects.get_or_create(
-                                infrastructure_type = pf_infrastructure_type,
-                                useful_storage_capacity = pf_useful_storage_capacity,
-                                cpu_number = pf_cpu_number,
-                                data_collection = pf_data_collection,
-                                cpu_hour_per_year = pf_cpu_hour_per_year,
-                                informatics_tools = pf_informatics_tools,
-                                users_number = pf_users_number,
-                                support_condition = pf_support_condition,
-                                server_description= pf_server_description,
-                                title_project_support = pf_title_project_support,
-                                description_projects_help = pf_description_projects_help,
-                                accompanied_project = pf_accompanied_project,
-                                hosted_projects = pf_hosted_projects,
-                                publications = pf_publications,
+                        Platform.objects.filter(name=pf_name).update(
+                            infrastructure_type = pf_infrastructure_type,
+                            useful_storage_capacity = pf_useful_storage_capacity,
+                            cpu_number = pf_cpu_number,
+                            data_collection = pf_data_collection,
+                            cpu_hour_per_year = pf_cpu_hour_per_year,
+                            informatics_tools = pf_informatics_tools,
+                            users_number = pf_users_number,
+                            support_condition = pf_support_condition,
+                            server_description= pf_server_description,
+                            title_project_support = pf_title_project_support,
+                            description_projects_help = pf_description_projects_help,
+                            accompanied_project = pf_accompanied_project,
+                            hosted_projects = pf_hosted_projects,
+                            publications = pf_publications,
                             )
 
-                        platform.save()
                         display_format = "\ninfrastructure, {}, has been saved."
                         print(display_format.format(platform))
 
