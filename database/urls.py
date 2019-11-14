3594ff548b17b96405732db4c856b5268290b541
lbouri <laurent.bouri@france-bioinformatique.fr>
2019-11-14 17:47:56 +0100

    models update

--- database/urls.py
+++ database/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('services', views.ServiceListView.as_view(), name='service-list'),
     path('services/<int:id>/', views.name_service, name='service-detail'),
+    path('tools/<int:id>/', views.name_tool, name='tool-detail'),
     path('list', views.service_list),
     path('search', views.search, name='search'),
 ]
