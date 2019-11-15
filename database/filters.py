2686f711f635018041e8066d3dc2a0912d44d457
lbouri <laurent.bouri@france-bioinformatique.fr>
2019-11-15 18:18:36 +0100

    models update

--- database/filters.py
+++ database/filters.py
@@ -1,4 +1,6 @@
 from .models import Service
+from .models import Tool
+from .models import Database
 import django_filters
 from django import forms
 
@@ -8,3 +10,19 @@ class ServiceFilter(django_filters.FilterSet):
     class Meta:
         model = Service
         fields = ['maturity', 'year_created', 'is_tool', 'is_data', 'is_training', 'is_compute', 'is_interoperability']
+
+
+class ToolFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Tool
+        fields = ['name']
+
+
+class DatabaseFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Database
+        fields = ['name']
